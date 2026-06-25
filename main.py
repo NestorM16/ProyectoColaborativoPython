8406b23f674c625a19f4e44a845783d1bc32b645
NestorM16 <nestor.menchaca@uip.edu.pa>
2026-06-24 22:43:02 -0500

    Agregado módulo matemático y archivo main

---
+++ main.py
@@ -0,0 +1,6 @@
+import matematicas
+
+print("Suma:", matematicas.suma(5, 3))
+print("Resta:", matematicas.resta(5, 3))
+print("Multiplicación:", matematicas.multiplicacion(5, 3))
+print("División:", matematicas.division(5, 3))
